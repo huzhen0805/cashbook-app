5cd5ffd9f5c87846ba5a63119593512390342fe0
huzhen0805 <940037451@qq.com>
2026-09-07 14:14:37 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = "cashbook-v1";
+const urlsToCache = ["./", "./ssq.html", "./manifest.json"];
+self.addEventListener("install", event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener("fetch", event => {
+    event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+});
